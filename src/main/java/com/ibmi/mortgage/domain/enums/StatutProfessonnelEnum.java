65f6cc1cda0888f347d16493cd75157e27231722
Cyril ROY <cyril.roy@fr.ibm.com>
2021-09-27 19:10:33 +0200

    projet coquille vide avant debut coding

--- src/main/java/com/ibmi/mortgage/domain/enums/StatutProfessonnelEnum.java
+++
@@ -1,9 +0,0 @@
-package com.ibmi.mortgage.domain.enums;
-
-public enum StatutProfessonnelEnum {
-
-    SALARIE_PRIVE,
-    SALARIE_PUBLIC,
-    AUTRE;
-
-}
--- src/main/java/com/ibmi/mortgage/domain/repo/EmprunteurRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ibmi.mortgage.domain.repo;
-
-import com.ibmi.mortgage.domain.entites.Emprunteur;
-
-public interface EmprunteurRepository {
-
-    Emprunteur getEmprunteur(String idEmprunteur);
-
-}
